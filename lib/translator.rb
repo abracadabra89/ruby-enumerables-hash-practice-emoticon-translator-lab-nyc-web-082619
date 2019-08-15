82b470a18bddb5e2c14601b46bc87360969ee331
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-15 22:36:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,6 @@
 # require modules here
 
-def load_library(file_path, emoticon)
+def load_library(file_path)
   # code goes here
   library = YAML.load_file(file_path)
   result = {"get_meaning" => {}, "get_emoticon" => {}}
