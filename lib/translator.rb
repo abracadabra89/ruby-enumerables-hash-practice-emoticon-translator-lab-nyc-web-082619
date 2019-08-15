edbd19cfb4c4816895d37dacc6fbde2dea856ddb
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-15 22:35:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,8 +8,7 @@ def load_library(file_path, emoticon)
       result["get_meaning"][emoticons[1]] = meaning
       result["get_emoticon"][emoticons[0]] = emoticons[1]
     end
-  end	
-  "Sorry, that emoticon was not found"
+  result
 
 end
 
