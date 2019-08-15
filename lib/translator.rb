4238df216f139e00d0336217e7815e9a2e0b005c
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-15 22:33:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,15 +2,14 @@
 
 def load_library(file_path, emoticon)
   # code goes here
-  hash = load_library(file_path)
-  hash["get_emoticon"].each do |eng,jap|	  hash["get_emoticon"].each do |eng,jap|
-    return jap if eng == emoticon	    if eng == emoticon
+  hash = load_library(file_path)	 
+  hash["get_emoticon"].each do |eng,jap|
+  if eng == emoticon	   
       return jap
       break
     end
-  end	  end
-  "Sorry, that emoticon was not found"	  "Sorry, that emoticon was not found"
-end	
+  end	
+  "Sorry, that emoticon was not found"
 
 end
 
